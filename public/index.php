d529d113f0e5d614b6175aae92564d56f76b84a5
Ed Kelly <me@edkelly.co.uk>
2013-07-06 18:05:22 +0000

    Adding mongo extension

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+phpinfo();
+exit;
+
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
