22dbeb96473e9dcfb595b88474af7dd69c39a1da
Ed Kelly <me@edkelly.co.uk>
2013-07-06 18:08:17 +0000

    Updating index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-phpinfo();
-exit;
-
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
