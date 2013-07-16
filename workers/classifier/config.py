21abd0cac7e48bdb3d499b57af17ca2f5f1c54ab
Ed Kelly <me@edkelly.co.uk>
2013-07-16 23:24:35 +0000

    Nearly working

--- workers/classifier/config.py
+++ workers/classifier/config.py
@@ -9,10 +9,10 @@ class Twitter:
 	
 	#prod
 
-	#consumer_key 	= "EXq1MWQVie2vG1TzxG1Sw"
-	#consumer_secret = "QFQU4o9GsJHw6E7HEi2gkQjBJmBg5v54JONZXNWfO0"
+	consumer_key 	= "EXq1MWQVie2vG1TzxG1Sw"
+	consumer_secret = "QFQU4o9GsJHw6E7HEi2gkQjBJmBg5v54JONZXNWfO0"
 
 	#dev
 
-	consumer_key 	= "oATCFNu2mH3SUClIC6TaA"
-	consumer_secret = "Y06pJj8eQFp2b9IuwGDJOk1aq1d13WM9DcjaJEU"
+	#consumer_key 	= "oATCFNu2mH3SUClIC6TaA"
+	#consumer_secret = "Y06pJj8eQFp2b9IuwGDJOk1aq1d13WM9DcjaJEU"
