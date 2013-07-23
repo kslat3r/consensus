509663483eabed04a4da0376acd999bea0e9ae76
Ed Kelly <me@edkelly.co.uk>
2013-07-23 00:27:05 +0000

    Fixing search results bug

--- public/src/util.js
+++ public/src/util.js
@@ -2,8 +2,22 @@ var Util = {
 
 	parseDate: function(s) {
 		var date 	= new Date(s * 1000);
-		var string 	= date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + date.getUTCDate() + '/' + (date.getUTCMonth() + 1) + '/' + date.getUTCFullYear();
+		var string 	= this.pad(date.getHours(), 2) + ':' + this.pad(date.getMinutes(), 2) + ':' + this.pad(date.getSeconds(), 2) + ' ' + this.pad(date.getUTCDate(), 2) + '/' + this.pad((date.getUTCMonth() + 1), 2) + '/' + date.getUTCFullYear();
 
 		return string;
-	}  
+	}
+
+	pad: function(num, len) {
+		if (num.length < len) {
+			str = ''
+
+			for (i = 0; i < len; i++) {
+				str += '0';
+			}
+
+			str += num;
+		}
+
+		return num;
+	} 
 }
